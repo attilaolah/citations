6845641865b7d3f63be6abfa8f2c58d027c43342
Attila Oláh <attila@dorn.haus>
2026-04-02 08:08:24 +0000

    nix: add flake-parts flake and lockfile

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Citations repository flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs @ {
+    flake-parts,
+    systems,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
+
+      perSystem = {pkgs, ...}: {
+        formatter = pkgs.alejandra;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            alejandra
+          ];
+        };
+      };
+    };
+}
