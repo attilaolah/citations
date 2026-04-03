345af2f7ded611e26dcfafa710c66d62c5c5f555
Attila Oláh <attila@dorn.haus>
2026-04-03 15:55:19 +0200

    fix: python3 packages

--- nix/overlays/python-docling.nix
+++ nix/overlays/python-docling.nix
@@ -1,28 +1,43 @@
-final: prev: {
-  python314Packages = prev.python314Packages.overrideScope (_pyFinal: pyPrev: {
-    docling = pyPrev.docling.overridePythonAttrs (_old: rec {
-      version = "2.84.0";
-      src = final.fetchPypi {
-        inherit version;
-        pname = "docling";
-        hash = "sha256-AHsLrTwOxF3JGvYIPL4fCpPd7xaGME9GbooWih+x3Ms=";
-      };
-    });
+final: prev: let
+  oldPackageOverrides =
+    if prev.python314 ? packageOverrides
+    then prev.python314.packageOverrides
+    else (_: _: {});
+in {
+  python314 = prev.python314.override {
+    packageOverrides = pyFinal: pyPrev:
+      (oldPackageOverrides pyFinal pyPrev)
+      // {
+        docling = pyPrev.docling.overridePythonAttrs (_old: let
+          version = "2.84.0";
+          hash = "sha256-AHsLrTwOxF3JGvYIPL4fCpPd7xaGME9GbooWih+x3Ms=";
+        in {
+          inherit version;
+          src = final.fetchPypi {
+            inherit hash version;
+            pname = "docling";
+          };
+        });
 
-    "docling-parse" = pyPrev."docling-parse".overridePythonAttrs (old: rec {
-      version = "5.7.0";
-      src = final.fetchPypi {
-        inherit version;
-        pname = "docling_parse";
-        hash = "sha256-x3IJwuCTyl+CZpUr0TuVrvCd+jjmmV7PhVlxgZeGyT0=";
+        "docling-parse" = pyPrev."docling-parse".overridePythonAttrs (old: let
+          version = "5.7.0";
+          hash = "sha256-x3IJwuCTyl+CZpUr0TuVrvCd+jjmmV7PhVlxgZeGyT0=";
+        in {
+          inherit version;
+          src = final.fetchPypi {
+            inherit hash version;
+            pname = "docling_parse";
+          };
+          buildInputs = old.buildInputs ++ [final.blend2d];
+          patches =
+            (old.patches or [])
+            ++ [
+              ../patches/docling-parse-explicit-json-conversions.patch
+            ];
+          meta = old.meta // {broken = false;};
+        });
       };
-      buildInputs = old.buildInputs ++ [final.blend2d];
-      patches =
-        (old.patches or [])
-        ++ [
-          ../patches/docling-parse-explicit-json-conversions.patch
-        ];
-      meta = old.meta // {broken = false;};
-    });
-  });
+  };
+
+  python314Packages = final.python314.pkgs;
 }
